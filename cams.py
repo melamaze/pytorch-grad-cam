75bbb80aa9cff2c88f5b12274ed1d6bb6bda567b
hentci <1222bruce@gmail.com>
2022-08-22 12:49:40 +0800

    [add] cams.py usage: --folder {input folder}

--- cams.py
+++ cams.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import numpy as np
 import torch
+import glob as glob
 from torchvision import models
 from pytorch_grad_cam import GradCAM, \
     HiResCAM, \
@@ -21,7 +22,8 @@ from pytorch_grad_cam.utils.image import show_cam_on_image, \
     deprocess_image, \
     preprocess_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-
+from torch.nn import functional as F
+from torch import topk
 from models import CNN_Model
 
 
@@ -29,11 +31,11 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--use-cuda', action='store_true', default=False,
                         help='Use NVIDIA GPU acceleration')
-    parser.add_argument(
-        '--image-path',
-        type=str,
-        default='./examples/both.png',
-        help='Input image path')
+    # parser.add_argument(
+    #     '--image-path',
+    #     type=str,
+    #     default='./examples/both.png',
+    #     help='Input image path')
     parser.add_argument('--aug_smooth', action='store_true',
                         help='Apply test time augmentation to smooth the CAM')
     parser.add_argument(
@@ -48,6 +50,7 @@ def get_args():
                                  'eigengradcam', 'layercam', 'fullgrad'],
                         help='Can be gradcam/gradcam++/scorecam/xgradcam'
                              '/ablationcam/eigencam/eigengradcam/layercam')
+    parser.add_argument('--folder', type=str, help='It will process all the pictures in this folder')
 
     args = parser.parse_args()
     args.use_cuda = args.use_cuda and torch.cuda.is_available()
@@ -109,52 +112,65 @@ if __name__ == '__main__':
     # from pytorch_grad_cam.utils.find_layers import find_layer_types_recursive
     # find_layer_types_recursive(model, [torch.nn.ReLU])
     target_layers = [model.conv]
-    print(target_layers)
+    # print(target_layers)
+    print('in', args.folder)
+    folder = args.folder + '/*'
+    for image_path in glob.glob(folder):
+        
+        rgb_img = cv2.imread(image_path, 1)[:, :, ::-1]
+        rgb_img = np.float32(rgb_img) / 255
+        input_tensor = preprocess_image(rgb_img,
+                                        mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225])
 
-    rgb_img = cv2.imread(args.image_path, 1)[:, :, ::-1]
-    rgb_img = np.float32(rgb_img) / 255
-    input_tensor = preprocess_image(rgb_img,
-                                    mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
+        # input_tensor = input_tensor.unsqueeze(0)
+        outputs = model(input_tensor)
+        probs = F.softmax(outputs).data.squeeze()
+        # get the class indices of top k probabilities
+        class_idx = topk(probs, 1)[1].int()
+        res = str(int(class_idx[0]))
+        print('The result of classification is -->', res)
 
-    # print('QQ')
-    # We have to specify the target we want to generate
-    # the Class Activation Maps for.
-    # If targets is None, the highest scoring category (for every member in the batch) will be used.
-    # You can target specific categories by
-    # targets = [e.g ClassifierOutputTarget(281)]
-    targets = None
+        # We have to specify the target we want to generate
+        # the Class Activation Maps for.
+        # If targets is None, the highest scoring category (for every member in the batch) will be used.
+        # You can target specific categories by
+        # targets = [e.g ClassifierOutputTarget(281)]
+        targets = None
 
-    # Using the with statement ensures the context is freed, and you can
-    # recreate different CAM objects in a loop.
-    cam_algorithm = methods[args.method]
-    with cam_algorithm(model=model,
-                       target_layers=target_layers,
-                       use_cuda=args.use_cuda) as cam:
+        # Using the with statement ensures the context is freed, and you can
+        # recreate different CAM objects in a loop.
+        cam_algorithm = methods[args.method]
+        with cam_algorithm(model=model,
+                        target_layers=target_layers,
+                        use_cuda=args.use_cuda) as cam:
 
-        # AblationCAM and ScoreCAM have batched implementations.
-        # You can override the internal batch size for faster computation.
-        cam.batch_size = 32
-        grayscale_cam = cam(input_tensor=input_tensor,
-                            targets=targets,
-                            aug_smooth=args.aug_smooth,
-                            eigen_smooth=args.eigen_smooth)
+            # AblationCAM and ScoreCAM have batched implementations.
+            # You can override the internal batch size for faster computation.
+            cam.batch_size = 32
+            grayscale_cam = cam(input_tensor=input_tensor,
+                                targets=targets,
+                                aug_smooth=args.aug_smooth,
+                                eigen_smooth=args.eigen_smooth)
 
-        # Here grayscale_cam has only one image in the batch
-        grayscale_cam = grayscale_cam[0, :]
+            # Here grayscale_cam has only one image in the batch
+            grayscale_cam = grayscale_cam[0, :]
 
-        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+            cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 
-        # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
-        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+            # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
+            cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
 
-    gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
-    gb = gb_model(input_tensor, target_category=None)
+        gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
+        gb = gb_model(input_tensor, target_category=None)
 
-    cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
-    cam_gb = deprocess_image(cam_mask * gb)
-    gb = deprocess_image(gb)
+        cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+        cam_gb = deprocess_image(cam_mask * gb)
+        gb = deprocess_image(gb)
 
-    cv2.imwrite(f'{args.method}_cam.jpg', cam_image)
-    cv2.imwrite(f'{args.method}_gb.jpg', gb)
-    cv2.imwrite(f'{args.method}_cam_gb.jpg', cam_gb)
+        cv2.imshow('CAM', cam_image/255.)
+        cv2.waitKey(0)
+        save_name = f"{image_path.split('/')[-1].split('.')[0]}"
+        cv2.imwrite(f'outputs/{args.method}_cam_{save_name}.jpg', cam_image)
+        # cv2.imwrite(f'{args.method}_gb.jpg', gb)
+        # cv2.imwrite(f'{args.method}_cam_gb.jpg', cam_gb)
